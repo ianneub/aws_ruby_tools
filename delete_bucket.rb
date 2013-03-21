c9a8d88a476b6bd67c2561541bad0f5f95301d03
Ian Neubert <ian@ianneubert.com>
2013-03-21 06:16:17 -0700

    Upgraded delete_bucket.rb to be multithreaded.

--- delete_bucket.rb
+++ delete_bucket.rb
@@ -1,21 +1,50 @@
 $: << File.dirname(__FILE__)
 require 'include'
 
-#WARNING: This will delete all keys in a bucket!!
+delete_bucket = ''
+prefix = ''
 
-source_bucket = ''
+# Thread management
+delete_threads = []
+delete_queue = Queue.new
+mutex_total = Mutex.new
+thread_count = 10
 
-$s3.incrementally_list_bucket(source_bucket) {|i|
+# Tracking variables
+total_delete_listed = 0
+total_deleted = 0
+
+# Gather keys to be deleted
+$s3.incrementally_list_bucket(delete_bucket, "prefix" => prefix) {|i|
   i[:contents].each do |key|
-    begin
-      puts "Deleting: #{source_bucket}/#{key[:key]}"
-      $s3.delete(source_bucket, key[:key])
-    rescue
-      sleep 5
-      retry
-    end
+    delete_queue.enq(key[:key])
+    total_delete_listed += 1
   end
 }
+thread_count.times {delete_queue.enq(:END_OF_BUCKET)}
 
-puts "Deleting bucket: #{source_bucket}"
-$s3.delete_bucket source_bucket
+# Deletion threads
+thread_count.times do |count|
+  delete_threads << Thread.new(count) do |number|
+    Thread.current[:number] = number
+    begin
+      key = delete_queue.deq
+      unless key == :END_OF_BUCKET
+        $s3.delete(delete_bucket, key)
+        # puts "Deleted: #{key}"
+        mutex_total.synchronize {total_deleted += 1}
+        puts "Deleted #{total_deleted} out of #{total_delete_listed}" if (rand(100) == 1)
+      end
+    end until (key == :END_OF_BUCKET)
+  end  
+end
+
+delete_threads.each do |t| 
+  begin
+    t.join
+  rescue RuntimeError => e
+    puts "Failure on thread #{t[:number]}: #{e.message}"
+  end
+end
+
+puts "Deleted #{total_deleted} out of #{total_delete_listed}"
